439e3d7d7e9fc4c9326f3af328c7b03f7be4f8b4
jam3sl1ng <jamesling596@gmail.com>
2025-12-04 18:23:44 +0000

    V0.3

--- Overworld.js
+++ Overworld.js
@@ -4,32 +4,36 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.map = null;
   }
 
+  startGameLoop() {
+    const step = () => {
+      // Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // Draw lower layer
+      this.map.drawLowerImage(this.ctx);
+
+      // Draw game objects
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.sprite.draw(this.ctx);
+      })
+
+      // Draw upper layer
+      this.map.drawUpperImage(this.ctx);
+
+      requestAnimationFrame(() => {
+        step();   
+      })    
+    }
+    step();
+  }
+
+
   init() {
-    // Background 'Demo Lower' area
-    const image = new Image();
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0)
-    };
-    image.src = "/images/maps/DemoLower.png";
-
-    // Place game objects
-    const hero = new GameObject({
-      x: 5,
-      y: 6
-    });
-    const npc = new GameObject({
-      x: 7,
-      y: 9,
-      src: "/images/characters/people/npc1.png"
-    });
-
-    // Draw game objects
-    setTimeout(() => {
-      hero.sprite.draw(this.ctx);
-      npc.sprite.draw(this.ctx);
-    }, 200)
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+    this.startGameLoop();
   }
 
 }
